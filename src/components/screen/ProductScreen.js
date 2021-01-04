788f869ee09b019711179f8976ea2ecf4184c216
shadan72 <shadanabdulkarim94@gmail.com>
2021-01-04 20:28:56 +0300

    Add: backend - API

--- src/components/screen/ProductScreen.js
+++ src/components/screen/ProductScreen.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import data from '../../data';
+import Rating from '../Rating';
 
 export default function ProductScreen(props) {
     const product=data.products.find( (imageId) => (imageId._id === props.match.params.id));
+    if(!product)
+    {
+        return (<div> Image not found</div>)
+    }
     return (
         <div>
+            <Link to="/"> Back</Link>
           <div className="row">
             <div> 
                <img src={product.image}  alt={product.name} />
             </div>
             <div>
-
+             <ul>
+                 <li> 
+                     <h1>{product.name}</h1>
+                 </li>
+                 <li>
+                     <Rating rating={product.rating}  numReviews={product.review}/>
+                 </li>
+                 <li>
+                     <h2>Price: ${product.price}</h2>
+                 </li>
+                 <li>{product.countOnStock >= 1 ? <span>In stock</span>: <span>Not in stock</span>}</li>
+             </ul>
+             <button> Add to Cart</button>
             </div>
 
-            <div>
-
+            <div >
+             
             </div>
           </div>
         </div>
